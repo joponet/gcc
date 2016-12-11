a567bf7b0f60f0170ff65193e50fa3f1efde6292
joponet <joponet@gmail.com>
2016-12-11 19:59:17 +0100

    sudoku.cc

---
+++ sudoku.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+typedef vector<int> Fila;
+typedef vector<Fila> Matriu;
+bool comprova(Matriu& matriu, int fo, int co, int ff, int cf) {
+	vector <int> llista(10);
+	for (int i=fo ; i<=ff ; i++) {
+		for (int j=co ; j<=cf ; j++) {
+			int valor=matriu[i][j];
+			llista[valor]++;
+			if (llista[valor]>1) return false;
+		}
+	}
+	return true;
+}
+int main() {
+	int n;
+	Matriu matriu(9,Fila(9));
+	cin >> n;
+	for (int x=0; x<n; x++) {
+		for (int i=0 ; i<9 ; i++) {
+			for (int j=0 ; j<9 ; j++) {
+				cin >> matriu[i][j];
+			}
+		}
+		bool esok=true;
+		for (int i=0 ; i<9 and esok ; i++) {
+			esok = comprova(matriu,i,0,i,8);
+			if (esok) esok = comprova(matriu,0,i,8,i);
+		}
+		for (int i=0 ; i<9 and esok ; i+=3) {
+			for (int j=0 ; j<9 and esok ; j+=3) {
+				esok = comprova(matriu,i,j,i+2,j+2);
+			}
+		}
+		if (esok) cout << "si" << endl;
+		else cout << "no" << endl;
+	}
+}
